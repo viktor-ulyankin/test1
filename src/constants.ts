7728b1b3eae0f050228d75c7043b7736d8877178
Viktor Ulyankin <freeman.vitek@gmail.com>
2025-02-12 23:33:32 +0300

    Add backend Interfaces and ITEMS_URL

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const ITEMS_URL = "https://jsonplaceholder.typicode.com/photos";
---
+++ src/models.ts
@@ -0,0 +1,9 @@
+export type IPhoto = {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+};
+
+export type IPhotoList = IPhoto[];
