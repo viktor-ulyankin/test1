79715774d80d7f3bfb59e70f94192250c35b216b
Viktor Ulyankin <freeman.vitek@gmail.com>
2025-02-13 23:53:49 +0300

    Move filter logic to useFilter hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styles from "./App.module.css";
 import { useFetch } from "./hooks/useFetch";
 import { List } from "./List";
 import { Category, IPostList } from "./models";
 import { ITEMS_URL } from "./constants";
-import { titleFilter } from "./helpers/titleFilter";
 import { Search } from "./Search";
 import { CategorySelect } from "./CategorySelect";
-import { categoryFilter } from "./helpers/categoryFilter";
+import { useFilter } from "./hooks/useFilter";
 
 const App: React.FC = () => {
   const { data, isLoading, errorMessage } = useFetch<IPostList>(ITEMS_URL);
   const [category, setCategory] = useState<Category | "">("");
   const [search, setSearch] = useState("");
-  const [categoryFilteredList, setCategoryFilteredList] = useState(data);
-  const [filteredList, setFilteredList] = useState(data);
 
-  useEffect(() => {
-    const filteredItems = category
-      ? (data || []).filter(categoryFilter(category))
-      : data;
-
-    setCategoryFilteredList(filteredItems);
-  }, [data, category]);
-
-  useEffect(() => {
-    const filteredItems = search
-      ? (categoryFilteredList || []).filter(titleFilter(search))
-      : categoryFilteredList;
-
-    setFilteredList(filteredItems);
-  }, [categoryFilteredList, search]);
+  const filteredList = useFilter(data, category, search);
 
   return (
     <div className={styles.root}>
---
+++ src/hooks/useFilter.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { Category, IPostList } from "../models";
+import { categoryFilter } from "../helpers/categoryFilter";
+import { titleFilter } from "../helpers/titleFilter";
+
+export const useFilter = (
+  data: IPostList | null,
+  category: Category | "",
+  title: string
+) => {
+  const [categoryFilteredList, setCategoryFilteredList] = useState(data);
+  const [titleFilteredList, setTitleFilteredList] = useState(data);
+
+  useEffect(() => {
+    const filteredItems = category
+      ? (data || []).filter(categoryFilter(category))
+      : data;
+
+    setCategoryFilteredList(filteredItems);
+  }, [data, category]);
+
+  useEffect(() => {
+    const filteredItems = title
+      ? (categoryFilteredList || []).filter(titleFilter(title))
+      : categoryFilteredList;
+
+    setTitleFilteredList(filteredItems);
+  }, [categoryFilteredList, title]);
+
+  return titleFilteredList;
+};
